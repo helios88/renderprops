5e9451e3c84dcb48db37847cf28ff082c20ba809
Henry <tarou.88@gmail.com>
2019-05-10 11:55:52 +0900

    created Toggle element

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Toggle from './ToggleRenderProps';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Toggle
+          render={({ toggle, on }) => (
+            <div>
+              {on && <h1>Hello</h1>}
+              <button onClick={toggle}>Show me</button>
+            </div>
+          )}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
